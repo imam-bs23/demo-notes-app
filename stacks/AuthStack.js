e016eb29b7e64c40c072493cc972310620f56c69
imam-bs23 <imam.hasan@brainstation-23.com>
2022-08-11 17:06:31 +0600

    Adding a billing API

--- stacks/AuthStack.js
+++ stacks/AuthStack.js
@@ -38,4 +38,8 @@ export function AuthStack({ stack, app }) {
     auth,
   };
 }
-// npx aws-api-gateway-cli-test --username='imamhasan@gmail.com' --password='aA11111111*' --user-pool-id='us-east-1_GfdSVGnZm' --app-client-id='56bfq7aoj7v3ao14olsrg4i69q' --cognito-region='us-east-1' --identity-pool-id='us-east-1:16e36079-5aed-4f7c-b312-93e80a8fcd9f' --invoke-url='https://wo6d2ne8s7.execute-api.us-east-1.amazonaws.com' --api-gateway-region='us-east-1' --path-template='/notes' --method='POST' --body='{"content":"hello world","attachment":"hello.jpg"}'
+// npx aws-api-gateway-cli-test --username='imamhasan@gmail.com' --password='aA11111111*' --user-pool-id='us-east-1_GfdSVGnZm' --app-client-id='56bfq7aoj7v3ao14olsrg4i69q' --cognito-region='us-east-1' --identity-pool-id='us-east-1:16e36079-5aed-4f7c-b312-93e80a8fcd9f' --invoke-url='https://wo6d2ne8s7.execute-api.us-east-1.amazonaws.com' --api-gateway-region='us-east-1' --path-template='/billing' --method='POST' --body='{"source":"tok_visa","storage":21}'
+
+// publishable key = pk_test_51IeKr8ApY1WzqxQsPnsfVP7aEldqnlxwthaWn7ToI95ReZep9gNLyoXTQxcvnhGYUzBfSNcJwlxYbeRD8LkfT2mI00NwcJUxIS
+
+// secret key = sk_test_51IeKr8ApY1WzqxQs3PICarguOcFoNE12ZPYt32EcU5aJ9dKSVdpUIdAMHhV8cSEvypNxVYZi3JxyRMrIcaJXFoel00xXWCD8ii
